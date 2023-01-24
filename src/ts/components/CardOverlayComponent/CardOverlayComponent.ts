52b385137071ab93f28098e4796707f52f588eb2
Inge Heeringa <inge.heeringa@gmail.com>
2023-01-24 22:46:24 +0100

    Add buttons to overlay component

--- src/ts/components/CardOverlayComponent/CardOverlayComponent.ts
+++ src/ts/components/CardOverlayComponent/CardOverlayComponent.ts
@@ -30,28 +30,44 @@ class CardOverlayComponent implements CardOverlayComponentStructure {
     if (this.character instanceof King) {
       this.element.innerHTML = `
       <ul class="list-unstyled">
-      <li>Years of Reign: ${this.character.yearsOfReign}</li>
+        <li>Years of Reign: ${this.character.yearsOfReign}</li>
       </ul>
+      <div class="character__actions">
+        <button class="character__action btn">speak</button>
+        <button class="character__action btn">die</button>
+      </div>
       `;
     } else if (this.character instanceof Fighter) {
       this.element.innerHTML = `
       <ul class="list-unstyled">
-      <li>Weapon: ${this.character.weapon}</li>
-      <li>Dexterity: ${this.character.dexterity}</li>
+        <li>Weapon: ${this.character.weapon}</li>
+        <li>Dexterity: ${this.character.dexterity}</li>
       </ul>
+        <div class="character__actions">
+        <button class="character__action btn">speak</button>
+        <button class="character__action btn">die</button>
+      </div>
       `;
     } else if (this.character instanceof Squire) {
       this.element.innerHTML = `
       <ul class="list-unstyled">
-      <li>Kiss Ass level: ${this.character.kissAssLevel}</li>
-      <li>Advises to: ${this.character.serves.characterData.name}</li>
+        <li>Kiss Ass level: ${this.character.kissAssLevel}</li>
+        <li>Advises to: ${this.character.serves.characterData.name}</li>
       </ul>
+        <div class="character__actions">
+        <button class="character__action btn">speak</button>
+        <button class="character__action btn">die</button>
+      </div>
       `;
     } else if (this.character instanceof Advisor) {
       this.element.innerHTML = `
       <ul class="list-unstyled">
-      <li>Serves to: ${this.character.advises.characterData.name}</li>
-      </ul>`;
+        <li>Serves to: ${this.character.advises.characterData.name}</li>
+      </ul>
+        <div class="character__actions">
+        <button class="character__action btn">speak</button>
+        <button class="character__action btn">die</button>
+      </div>`;
     }
   }
 }
